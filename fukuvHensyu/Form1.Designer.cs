53a94a429a088c64889e2b2107025f298b42474c
A601-01 <tnk.dat.class@gmail.com>
2022-06-16 16:10:24 +0900

    ugoku

--- fukuvHensyu/Form1.Designer.cs
+++ fukuvHensyu/Form1.Designer.cs
@@ -36,12 +36,13 @@ namespace fukuvHensyu
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(303, 108);
+            this.label1.Location = new System.Drawing.Point(264, 114);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 0;
@@ -93,11 +94,22 @@ namespace fukuvHensyu
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(532, 164);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(76, 41);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(693, 424);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -105,6 +117,7 @@ namespace fukuvHensyu
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,6 +131,7 @@ namespace fukuvHensyu
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- fukuvHensyu/Form1.cs
+++ fukuvHensyu/Form1.cs
@@ -14,6 +14,7 @@ namespace fukuvHensyu
     {
         int vx = 0;
         int vy = 0;
+        
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +23,26 @@ namespace fukuvHensyu
         {
             label1.Left += vx;
             label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = 10;
+            }
+
+            if(label1.Top < 0)
+            {
+                vy = 10;
+            }
+
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -10;
+            }
+
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -10;
+            }
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -52,6 +73,14 @@ namespace fukuvHensyu
             MessageBox.Show("荒川　龍");
         }
 
-       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            vx = -vx;
+        }
     }
 }
